b4a4aa9cc276fada113a99a0618413c1dd4d9d4a
Norberto Boa <nboa.26@gmail.com>
2025-04-07 21:11:48 +0200

    ADD: Create user account controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PrismaService } from './prisma/prisma.service'
+import { CreateAcccountController } from './controllers/create-account.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [CreateAcccountController],
+  providers: [PrismaService],
 })
 export class AppModule {}
