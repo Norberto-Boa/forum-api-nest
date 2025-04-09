b31dd847a1e50775375114fdbe5ad47e9aad889f
Norberto Boa <nboa.26@gmail.com>
2025-04-09 14:46:04 +0200

    Add: Finish authentication controller

--- src/controllers/authenticate-controller.ts
+++ src/controllers/authenticate-controller.ts
@@ -1,14 +1,53 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
 
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
 
   @Post()
-  async handle() {
-    const token = this.jwt.sign({ sub: 'Love u' });
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body;
 
-    return token;
+    const userExists = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!userExists) {
+      throw new UnauthorizedException('Credentials are wrong!');
+    }
+
+    const passwordMatches = await compare(password, userExists.password);
+
+    if (!passwordMatches) {
+      throw new UnauthorizedException('Credentials are wrong!');
+    }
+
+    const accessToken = this.jwt.sign({ sub: userExists.id });
+
+    return { access_token: accessToken };
   }
 }
