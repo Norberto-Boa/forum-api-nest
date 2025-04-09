5de61f5c96463fc17afe99113e65c400b4e27b9a
Norberto Boa <nboa.26@gmail.com>
2025-04-09 16:34:11 +0200

    ADD: create new question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,13 +1,70 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import type { TokenPayload } from 'src/auth/jwt.strategy';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+
   @Post()
-  handle(@CurrentUser() user: TokenPayload) {
-    return user;
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    const { title, content } = body;
+
+    const userExists = this.prisma.user.findUnique({
+      where: {
+        id: user.sub,
+      },
+    });
+
+    if (!userExists) {
+      throw new UnauthorizedException(
+        'Must be a real user to create a question',
+      );
+    }
+
+    const question = await this.prisma.question.create({
+      data: {
+        title,
+        content,
+        slug: this.convertToSlug(title),
+        authorId: user.sub,
+      },
+    });
+
+    return { question };
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-') // Remove whitespaces
+      .replace(/[^\w-]+/g, '') // Get everything that isn't a word
+      .replace(/_/g, '-') // Get all underscore and change to hifen
+      .replace(/--+/g, '-') // Remove duplicates hifens
+      .replace(/-$/g, ''); // ;
   }
 }
