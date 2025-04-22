579aa0be8960e81d241a7ac5e2ab1f6c4ffa6347
Norberto Boa <nboa.26@gmail.com>
2025-04-22 13:38:28 +0200

    Isolate databases on e2e tests

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -3,12 +3,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['**/*.e2e-spec.ts'],
     globals: true,
     alias: {
       '@src': './src',
       '@test': './test',
     },
     root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
   },
   resolve: {
     alias: {
